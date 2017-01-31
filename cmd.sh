1bed732c7455b989fbd1c4fafae02f126effd52a
coolcow <mail@coolcow.org>
2017-01-31 14:19:09 +0100

    reconnect instead of not keeping alive if check fails

--- cmd.sh
+++ cmd.sh
@@ -1,5 +1,22 @@
 #!/bin/sh
 
+tunnel_up () { 
+  list of commands
+
+  # wait a little bit (to make sure ipsec is fully started)
+  sleep ${WAIT_AFTER_START}
+
+  # try to ipsec up the profile until success
+  until ipsec up ${PROFILE_NAME}
+  do 
+    sleep 1
+  done
+}
+
+tunnel_down() {
+  ipsec down ${PROFILE_NAME}
+}
+
 # adding a loopback alias for the tunnel ip of the client
 ip addr add ${LOCAL_IP} dev lo label lo:${PROFILE_NAME}
 
@@ -10,23 +27,18 @@ install -D -m 644 /install/ipsec.secrets /etc/ipsec.secrets
 # start ipsec
 ipsec start
 
-# wait a little bit (to make sure ipsec is fully started)
-sleep ${WAIT_AFTER_START}
+# connect tunnel
+tunnel_up
 
-# try to ipsec up the profile until success
-until ipsec up ${PROFILE_NAME}
-do 
-  sleep 1
-done
-
-# keep alive check
-if [ ! -z "$KEEPALIVE_CHECK" ]
-then
+if [ ! -z "$RECONNECT_IF_NOT" ]; then
   tail -f /var/log/charon.log &
   # keep alive
-  eval 'sleep $KEEPALIVE_CHECK_INTERVAL; while $KEEPALIVE_CHECK &> /dev/null; do sleep $KEEPALIVE_CHECK_INTERVAL; done'
+  eval 'while true; do if [ ! $RECONNECT_IF_NOT ]; then tunnel_down; tunnel_up; fi; sleep $RECONNECT_IF_NOT_INTERVAL; done'
 else
   # keep alive
   tail -f /var/log/charon.log
 fi
+
+# disconnect tunnel
+tunnel_down
 
